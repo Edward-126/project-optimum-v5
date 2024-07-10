8f9ef074982b34653ff4b922c9f66d06baa4f21d
Edward-126 <edwardhyde20126@gmail.com>
2024-07-10 22:30:57 +0530

    feat: vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -44,6 +46,7 @@ export default function RootLayout({
             {children}
           </div>
         </main>
+        <Analytics />
       </body>
     </html>
   );
